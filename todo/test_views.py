856bccd0b5adb28dfe76cd60fdd09177b871e6e1
Kpokc <pavmakarovs@gmail.com>
2021-08-19 09:10:16 +0000

    Preparation for Testing

---
+++ todo/test_views.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+# Create your tests here.
+class TestDjango(TestCase):
+
+    def test_this_works1(self):
+        self.assertEquals(1,0)
+    
+    def test_this_works2(self):
+        self.assertEquals(1,1)
+    
+    def test_this_works3(self):
+        self.assertEquals(1, )
